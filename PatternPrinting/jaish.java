88de71d1084a2595092e70de59e6b75e952bd4a4
Sriram4n <sriramthegamer2006@gmail.com>
2025-10-24 13:16:31 +0530

    ok

--- PatternPrinting/jaish.java
+++ PatternPrinting/jaish.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
-public class Pattern14{
+/*
+1 2 3 4 5 6
+ 2 3 4 5 6
+  3 4 5 6
+   4 5 6 
+    5 6
+     6
+    5 6
+   4 5 6 
+  3 4 5 6
+ 2 3 4 5 6
+1 2 3 4 5 6
+ */
+public class jaish {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number:");
+        System.out.println("Enter num ");
         int n = scan.nextInt();
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j < i; j++){
+            for(int j = 1; j < i ; j++){
                 System.out.print(" ");
             }
             for(int j = i; j <= n; j++){
@@ -13,8 +26,8 @@ public class Pattern14{
             }
             System.out.println();
         }
-        for(int i = n - 1; i > 0; i--){
-            for(int j = 1; j < i; j++){
+        for(int i =  n - 1; i > 0; i--){
+            for(int j = 1; j < i ; j++){
                 System.out.print(" ");
             }
             for(int j = i; j <= n; j++){
